5907c8152d37a5f98be76209f0560edd64ffc3e0
Fertz <145053493+firhzmelo@users.noreply.github.com>
2025-10-13 07:30:23 -0600

    Update win_home.py

--- esqueleto_app/src/app/win_home.py
+++ esqueleto_app/src/app/win_home.py
@@ -1,21 +1,26 @@
 import tkinter as tk
+from src.core.classes import User
+
+from .win_progress import open_win_progress
+from .win_talleres import open_win_talleres
+from .
+
 from tkinter import ttk, messagebox
-import webbrowser
 
-def open_win_home(parent: tk.Tk):
-    win = tk.Toplevel(parent)
-    win.title("Home / Bienvenida")
-    win.geometry("400x300")
-    frm = ttk.Frame(win, padding=16)
+def open_win_home(user:User):
+    root = tk.Tk()
+    root.title("Home / Bienvenida")
+    root.geometry("360x220")
+    frm = ttk.Frame(root, padding=16)
     frm.pack(fill="both", expand=True)
-
-    ttk.Label(frm, text="Talleres", font=("Segoe UI", 11, "bold")).pack(pady=(0, 8))
-    ttk.Button(frm, text="Tutorial huerto vertical",
-           command=lambda:webbrowser.open("https://www.youtube.com/watch?v=IEX7D8wxkm4")).pack()
-    ttk.Button(frm, text="Taller de composta",
-           command=lambda:webbrowser.open("https://educacion.mma.gob.cl/educacion-ambiental-en-tu-casa/compostaje/")).pack()
-    ttk.Button(frm, text="Tutorial papel reciclado",
-           command=lambda:webbrowser.open("https://youtu.be/kjkry2AkcPQ?si=dt_3K-GP22iAtmzg")).pack()
-    ttk.Button(frm, text="Talleres precenciales en León, Guanajuato",
-           command=lambda:webbrowser.open("https://www.leon.gob.mx/medioambiente/articulo.php?a=312")).pack()
-    ttk.Button(frm, text="Cerrar", command=win.destroy).pack(pady=8)
+    ttk.Label(frm, text=f"Hola, {user.name}", font=("Segoe UI", 11, "bold")).pack(pady=(0, 8))
+    ttk.Button(frm, text="Metas",
+               command=lambda: open).pack()
+    ttk.Button(frm, text="Progreso",
+               command=lambda: open_win_progress(root, user)).pack()
+    ttk.Button(frm, text="Talleres",
+               command=lambda: open_win_talleres()).pack()
+    ttk.Button(frm, text="Tips",
+               command=lambda: messagebox.showinfo("Info", "¡Equipo listo!")).pack()
+    ttk.Button(frm, text="Cerrar", command=root.destroy).pack()
+    root.mainloop()
