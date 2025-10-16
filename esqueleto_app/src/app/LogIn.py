d615bc1e5e0e6487c46871a42c757dddbed8c492
CesarOtgMtz <A00575262@tec.mx>
2025-10-16 12:57:24 -0600

    Refactor LogIn.py to use ttkbootstrap
    Refactor LogIn.py to use ttkbootstrap for improved UI and styling. Update button commands and window configurations.

--- esqueleto_app/src/app/LogIn.py
+++ esqueleto_app/src/app/LogIn.py
@@ -1,31 +1,37 @@
-import tkinter as tk
-from tkinter import ttk
-from .CreateAccount import crea_cuenta
-from src.core.leer_usuario import leer_usuario
-from .win_home import open_win_home
-from src.core.classes import User
+import ttkbootstrap as ttk
+from CreateAccount import crea_cuenta
+from core.leer_usuario import leer_usuario
+from win_home import open_win_home
+from core.classes import User
+
+tamLetra = 10
+tipLetra = "Verdana"
+tipTitulo = "Century Gothic"
+
 
 def dirige(user:User):
-    LogIn.destroy()
-    open_win_home(user)
+    win.destroy()
+    frame.pack_forget()
+    open_win_home(LogIn, user)
 
 def emergente(titulo, mensaje, dirigir, user:User = None):
     win.title(titulo)
-    win.geometry("300x150")
+    win.geometry("300x300")
 
     frame = ttk.Frame(win, padding=12)
     frame.pack(fill="both", expand=True)
 
-    tk.Label(frame, text=mensaje).pack()
+    ttk.Label(frame, text=mensaje, font=(tipLetra, tamLetra, "bold")).pack()
     if dirigir:
-        tk.Button(frame, text="Ok", command=dirige(user)).pack()
+        ttk.Button(frame, text="Ok", command=lambda u=user: dirige(u)).pack()
     else:
-        tk.Button(frame, text="Ok", command= lambda: win.destroy()).pack()
+        ttk.Button(frame, text="Ok", command= lambda: win.destroy()).pack()
 
 def check(parent ,usuario, clave):
     user = leer_usuario(usuario)
     global win
-    win = tk.Toplevel(parent)
+    global win 
+    win = ttk.Toplevel(parent)
 
     if user == -1:
         emergente("Usuario Incorrecto", "El usuario no existe o es incorrecto,\nintente de nuevo o cree una cuenta", 0)
@@ -38,24 +44,29 @@ def check(parent ,usuario, clave):
 
 def logIn():
     global LogIn
-    LogIn = tk.Tk()
-    LogIn.geometry("300x250")
-    LogIn.title("LogIn")
+    LogIn = ttk.Window(themename="minty")
 
+    s=ttk.Style()
+    s.configure("TButton", font=("Verdana",10, "bold"))
+
+
+    LogIn.geometry("500x900")
+    LogIn.title("LogIn")
+    global frame
     frame = ttk.Frame(LogIn, padding=16)
     frame.pack(fill="both", expand=True)
 
-    ttk.Label(frame, text="LogIn", font=("Segoe UI",16, "bold")).pack(pady = 10)
+    ttk.Label(frame, text="LogIn", font=("Century Gothic",30, "bold")).pack(pady = (150, 10))
 
-    usuario = tk.StringVar()
-    clave = tk.StringVar()
+    usuario = ttk.StringVar()
+    clave = ttk.StringVar()
 
-    ttk.Label(frame, text="Usuario").pack(fill = 'x')
-    ttk.Entry(frame, textvariable=usuario).pack(pady = 5, fill = 'x')
-    ttk.Label(frame, text="Contraseña").pack(fill='x')
-    ttk.Entry(frame, textvariable=clave, show='*').pack( pady= 5, fill ='x')
+    ttk.Label(frame, text="Usuario", font=("Verdana",tamLetra)).pack(fill = 'x', pady=15, padx=15)
+    ttk.Entry(frame, textvariable=usuario).pack(pady = 5, fill = 'x', padx=15)
+    ttk.Label(frame, text="Contraseña",font=("Verdana",tamLetra)).pack(fill='x', pady=15, padx=15)
+    ttk.Entry(frame, textvariable=clave, show='*').pack( pady= 15, fill ='x', padx=15)
 
-    ttk.Button(frame, text="Ingresar", command=lambda: check(LogIn, usuario.get(), clave.get())).pack()
-    ttk.Button(frame, text="Crear cuenta", command=lambda: crea_cuenta(LogIn)).pack()
+    ttk.Button(frame, text="Ingresar", width=50,command=lambda: check(LogIn, usuario.get(), clave.get())).pack( padx=10, pady=20)
+    ttk.Button(frame, text="Crear cuenta", width=50, command=lambda: crea_cuenta(LogIn)).pack( padx = 10, pady=20)
 
     LogIn.mainloop()
