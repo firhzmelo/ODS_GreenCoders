95501f12fde224b7e833bf8f354f432f577dbe6e
CesarOtgMtz <A00575262@tec.mx>
2025-10-16 12:58:56 -0600

    Refactor win_home.py to enhance functionality

--- esqueleto_app/src/app/win_home.py
+++ esqueleto_app/src/app/win_home.py
@@ -1,26 +1,67 @@
-import tkinter as tk
-from src.core.classes import User
+import ttkbootstrap as ttk
+from core.classes import User
+from PIL import Image, ImageTk
 
-from .win_progress import open_win_progress
-from .win_talleres import open_win_talleres
-from .
+from win_progress import open_win_progress
+from win_talleres import open_win_talleres
+from win_tips import open_win_tips
+from win_metas import open_win_metas
+from core.classes import Task
+from win_api import open_win_api
 
-from tkinter import ttk, messagebox
+tamLetra = 10
+tipLetra = "Verdana"
+tipTitulo = "Century Gothic"
 
-def open_win_home(user:User):
-    root = tk.Tk()
+def metas(frm:ttk.Frame, user:User):
+    # frm.pack_forget()
+    open_win_metas(root, user=user)
+
+
+def open_win_home(LogIn, user:User):
+    global root 
+    root = LogIn
     root.title("Home / Bienvenida")
-    root.geometry("360x220")
+    root.geometry("500x900")
+
+    s=ttk.Style()
+    s.configure("TButton", font=("Verdana",10, "bold"))
+    s.configure("Titulo.TLabel", font=(tipLetra, 20, "bold"))
+    s.configure("TLabel", font=(tipLetra, tamLetra))
+
     frm = ttk.Frame(root, padding=16)
     frm.pack(fill="both", expand=True)
-    ttk.Label(frm, text=f"Hola, {user.name}", font=("Segoe UI", 11, "bold")).pack(pady=(0, 8))
+
+    user.tasks.append(Task(
+        name='Ducha Express',
+        desc='Dúchate en el tiempo que dura una canción (máximo 5 minutos).'
+    ))
+    user.tasks.append(Task(
+        name='Cero Papel en tu Cartera',
+        desc='Hoy no aceptes ningún recibo impreso. Pídelo por correo o simplemente recházalo. '
+    ))
+    user.tasks.append(Task(
+        name='Movilidad Activa',
+        desc='Realiza un trayecto corto caminando o en bicicleta en lugar de usar el auto.'
+    ))
+
+    imagen = Image.open("Logo_App.png")
+    imagen = imagen.resize((300,118))
+
+    img_tk = ImageTk.PhotoImage(imagen)
+
+    ttk.Label(frm, image=img_tk).pack()
+
+    ttk.Label(frm, text=f"Hola, {user.name}", style="Titulo.TLabel").pack(pady=(10, 50))
     ttk.Button(frm, text="Metas",
-               command=lambda: open).pack()
+               command=lambda: metas(frm, user), width=100).pack(pady=15)
     ttk.Button(frm, text="Progreso",
-               command=lambda: open_win_progress(root, user)).pack()
+               command=lambda: open_win_progress(root, user=user), width=100).pack(pady=15)
     ttk.Button(frm, text="Talleres",
-               command=lambda: open_win_talleres()).pack()
+               command=lambda: open_win_talleres(root), width=100).pack(pady=15)
     ttk.Button(frm, text="Tips",
-               command=lambda: messagebox.showinfo("Info", "¡Equipo listo!")).pack()
-    ttk.Button(frm, text="Cerrar", command=root.destroy).pack()
+               command=lambda: open_win_tips(root), width=100).pack(pady=15)
+    ttk.Button(frm, text="Calidad del aire",
+               command=lambda: open_win_api(root), width=100).pack(pady=15)
+    ttk.Button(frm, text="Cerrar", command=root.destroy).pack(pady=15)
     root.mainloop()
